43ae1604e662f4cfbbb112b5b67809d9d80fc55c
Heba Omar <97067717+hebaomar94@users.noreply.github.com>
2023-06-23 12:05:38 +0300

    Create index.swift

---
+++ index.swift
@@ -0,0 +1,73 @@
+struct BankAccount {
+    
+    // Static Properties
+    
+    static let depositBonusRate = 0.01
+    
+    // Private Properties
+    
+    private var balance: Double = 0 {
+        didSet {
+            if balance < 100 {
+                displayLowBalanceMessage()
+            }
+        }
+    }
+    
+    private let password: String
+    
+    // Initializers
+        
+    init(initialDeposit: Double, password: String) {
+        self.password = password
+        makeDeposit(ofAmount: initialDeposit)
+    }
+    
+    // Internal Methods
+    
+    mutating func makeDeposit(ofAmount depositAmount: Double) {
+        let depositWithBonus = finalDepositWithBonus(fromInitialDeposit: depositAmount)
+        print("Making a $\(depositWithBonus) deposit")
+        self.balance += depositWithBonus
+    }
+    
+    mutating func makeWithdrawal(ofAmount withdrawalAmount: Double, usingPassword password: String) {
+        if !isValid(password) {
+            print("Error: Invalid password. Cannot make withdrawal.")
+            return
+        }
+        print("Making a $\(withdrawalAmount) withdrawal.")
+        self.balance -= withdrawalAmount
+    }
+    
+    func displayBalance(usingPassword password: String) {
+        if !isValid(password) {
+            print("Error: Invalid password. Cannot retrieve balance.")
+            return
+        }
+        print("Your current balance is $\(balance).")
+    }
+    
+    // Private Methods
+ 
+    private func finalDepositWithBonus(fromInitialDeposit deposit: Double) -> Double {
+        return deposit + (deposit * BankAccount.depositBonusRate)
+    }
+    
+    private func isValid(_ enteredPassword: String) -> Bool {
+        return password == enteredPassword
+    }
+    
+    private func displayLowBalanceMessage() {
+        print("Alert: Your balance is under $100.")
+    }
+}
+ 
+var myAccount = BankAccount(initialDeposit: 500, password: "yummy1")
+myAccount.makeDeposit(ofAmount: 50)
+myAccount.displayBalance(usingPassword: "yummy1")
+myAccount.makeWithdrawal(ofAmount: 500, usingPassword: "yummy12")
+myAccount.makeWithdrawal(ofAmount: 500, usingPassword: "yummy1")
+myAccount.displayBalance(usingPassword: "yummy1")
+myAccount.makeDeposit(ofAmount: 100)
+myAccount.displayBalance(usingPassword: "yummy1")
